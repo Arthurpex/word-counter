8d1e264701803b7c76feca4cb4b7b9cf7a6f0741
Gustavo Ortiz <gustavo.egert.ortiz@gmail.com>
2021-08-05 15:37:59 -0300

    fix

--- sequential.c
+++ sequential.c
@@ -6,48 +6,11 @@
 
 #define BUFFER_SIZE 1000
 
-int contaOcorrencias(const char *word);
-
-int main(int argc, char const *argv[])
-{   
-    if (argc < 2){
-        printf("Not enough arguments to start program\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int i;
-    int contagens[4] = {0, 0, 0, 0};
-    int qt_palavras;
-
-    qt_palavras = atoi(argv[1]);
-
-    if (qt_palavras > 10){
-        printf("Quantidade de palavras a serem pesquisadas não pode execeder 10\n");
-        exit(EXIT_FAILURE);
-    }
-
-
-    char palavras[10][30] = {"Hobbit", "ring", "Bilbo", "Gandalf", "yes", "no", "why", "because", "are", "you"};
-
-    printf("Programa contador de palavras, Resultado: \n");
-    printf("num_ocorrencias -> palavra \n");
-    for (i=0; i < qt_palavras; i++){
-        contagens[i] = contaOcorrencias(palavras[i]);
-        printf(" %d -> '%s'\n", contagens[i], palavras[i]);
-    }
-
-    return 0;
-}
-
-
-
-int contaOcorrencias(const char *palavra)
-{   
+int contaOcorrencias(const char *palavra) {   
     FILE *arquivo;
     arquivo = fopen("words.txt", "r");
 
-    if (arquivo == NULL)
-    {
+    if (arquivo == NULL) {
         printf("Não foi possível abrir o arquivo.\n");
 
         exit(EXIT_FAILURE);
@@ -60,17 +23,12 @@ int contaOcorrencias(const char *palavra)
     
     count = 0;
 
-    // Le linha a linha até fim do arquivo
-    while ((fgets(str, BUFFER_SIZE, arquivo)) != NULL)
-    {
+    // Lê linha a linha até fim do arquivo
+    while ((fgets(str, BUFFER_SIZE, arquivo)) != NULL) {
         index = 0;
 
-        // acha proxima ocorrencia da palavra em str
-        while ((pos = strstr(str + index, palavra)) != NULL)
-        {
-            // Index of word in str is
-            // Memory address of pos - memory
-            // address of str.
+        // Acha próxima ocorrência da palavra em string
+        while ((pos = strstr(str + index, palavra)) != NULL) {
             index = (pos - str) + 1;
             count++;
         }
@@ -78,4 +36,34 @@ int contaOcorrencias(const char *palavra)
 
     fclose(arquivo);
     return count;
+}
+
+int main(int argc, char const *argv[]) {   
+    
+    if (argc < 2) {
+        printf("Não há número suficiente de argumentos para iniciar o programa\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int qt_palavras = atoi(argv[1]);
+
+    int i;
+    int contagens[qt_palavras];
+
+    if (qt_palavras > 10) {
+        printf("Quantidade de palavras a serem pesquisadas não pode execeder 10\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    char palavras[10][30] = {"Hobbit", "ring", "Bilbo", "Gandalf", "yes", "no", "why", "because", "are", "you"};
+
+    printf("Programa contador de palavras, Resultado: \n");
+    printf("num_ocorrencias -> palavra \n");
+    for (i=0; i < qt_palavras; i++) {
+        contagens[i] = contaOcorrencias(palavras[i]);
+        printf(" %d -> '%s'\n", contagens[i], palavras[i]);
+    }
+
+    return 0;
 }
